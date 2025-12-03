c77377e6620bb4af669ee7ff8e35d7ce4154dcb2
Flávio Ghiraldeli Botelho De Paula <ghiraldeli26@gmail.com>
2025-12-03 13:49:42 -0300

    Using useRef() to react to user clicks

--- src/components/Dialog/index.jsx
+++ src/components/Dialog/index.jsx
@@ -1,23 +1,24 @@
-import React from "react";
+import React, { useRef } from "react";
 import './dialog.style.css'
 
 export function Dialog() {
     //não deveríamos fazer buscas no DOM desse jeito!
     //const dialog = document.querySelector("dialog");
-  
+    
+    const dialogRef = useRef(null)
 
     // "Show the dialog" button opens the dialog modally
     const openDialog = () => {
-        dialog.showModal();
+        dialogRef.current.showModal();
     };
 
     // "Close" button closes the dialog
     const closeDialog = () => {
-        dialog.close();
+        dialogRef.current.close();
     };
     return (
         <>
-            <dialog>
+            <dialog ref={dialogRef}>
                 <button autoFocus onClick={closeDialog}>Close</button>
                 <p>This modal dialog has a groovy backdrop!</p>
             </dialog>
